132e5d218f04e6804ab0d466a1066edfbc7b99ba
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-12 14:17:41 -0300

    Add $fillable in Book Model

--- app-back/app/Http/Controllers/BookController.php
+++ app-back/app/Http/Controllers/BookController.php
@@ -3,28 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\models\Book;
+use App\models\Book;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Carbon;
 
 class BookController extends Controller
 {
     protected $rules = [
         'name' => 'required|string|max:30',
         'author' => 'required|string|max:30',
-        'edition_date' => 'required|date|date_format:d/m/Y|before:today',
+        'edition_date' => 'required|date|date_format:Y-m-d|before:today',
        
     ];
 
     protected $messages = [
         'name.required' => 'El nombre es requerido.',
-        'autor.required' => 'El autor es requerido.',
+        'author.required' => 'El autor es requerido.',
         'edition_date.required' => 'La fecha de edicion es requerida.',
         'name.string' => 'El name debe ser una cadena de texto.',
         'autor.string' => 'El autor debe ser una cadena de texto.',
         'edition_date.date' => 'La fecha de edicion debe ser una fecha.',
         'name.max' => 'El nombre no puede tener mas de 30 caracteres.',
         'author.max' => 'El nombre no puede tener mas de 30 caracteres.',
-        'edition_date.date_format' => 'La fecha de Edición debe tener el formato dd/mm/yyyy.',
+        'edition_date.date_format' => 'La fecha de Edición debe tener el formato Y-m-d.',
         'edition_date.before' => 'La fecha de edicion no puede ser posterior a la fecha actual'
     ];
 
@@ -53,13 +54,13 @@ class BookController extends Controller
         $validator = Validator::make($request->all(), $this->rules, $this->messages);
 
         
-        if ($validator->fails()) 
+        if ($validator->fails())
             return response()->json(['errors' => $validator->errors()], 400);
         
         else {
             $book = new Book( ['name' => $request->get('name'),
-                              'autor' => $request->get('author'),
-                              'edition_date' => $request->get('$edition_date'), 
+                              'author' => $request->get('author'),
+                              'edition_date' => $request->get('edition_date')
                              ]);
 
             $book->save();
--- app-back/routes/api.php
+++ app-back/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::get('books', [BookController::class, 'index']);
+    Route::post('books', [BookController::class, 'create']);
