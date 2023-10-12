c21507d1037ac90a3f6db6e87ec3e084adb1b9a7
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-12 14:46:30 -0300

    Add route to get book by id

--- app-back/routes/api.php
+++ app-back/routes/api.php
@@ -18,4 +18,5 @@ use App\Http\Controllers\BookController;
 */
 
     Route::get('books', [BookController::class, 'index']);
+    Route::get('book/{id}', [BookController::class, 'show']);
     Route::post('books', [BookController::class, 'create']);
