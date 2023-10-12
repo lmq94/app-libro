ce5ab0e48fd0a21f1f2bbe3acfac77c2bd253a6f
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-12 11:44:17 -0300

    Updated migration of the book entity

--- app-back/database/migrations/2023_10_11_215847_create_books_table.php
+++ app-back/database/migrations/2023_10_11_215847_create_books_table.php
@@ -4,22 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
+
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->String('name', 30);
+            $table->String('author', 30);
+            $table->date('edition_date');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+   
     public function down(): void
     {
         Schema::dropIfExists('books');
