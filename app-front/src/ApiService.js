5bfa8a06c0a9eae9744cae7c6cb383a28f76e984
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-13 20:44:40 -0300

    Created deleteBook method

--- app-front/src/ApiService.js
+++ app-front/src/ApiService.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://localhost:8000/api';
 
 
 function getBooks(setItems){
-    axios.get(`${baseUrl}/$books`)
+    axios.get(`${baseUrl}/books`)
         .then(response => {
             console.log(response.data);
             setItems(response.data);
@@ -15,4 +15,18 @@ function getBooks(setItems){
 
 }
 
-export {getBooks}
+function deleteBook(id){
+    axios.delete(`${baseUrl}/book/${id}`).
+    then(response => {
+        if (response.status === 204) {
+          console.log(`Libro con ID ${id} eliminado exitosamente.`);
+        } else {
+          console.error(`Error al eliminar el libro con ID ${id}.`);
+        }
+    })
+    .catch(error => {
+        console.error(`Error en la solicitud de eliminación: ${error}`);
+      });
+}
+
+export {getBooks, deleteBook}
