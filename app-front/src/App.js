30a6019a897c760056d334cd645c068dc4e853e0
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-14 14:48:48 -0300

    Implemented Router

--- app-front/src/App.js
+++ app-front/src/App.js
@@ -1,14 +1,27 @@
 
 import './App.css';
 import Crud from './Crud';
+import {Navbar, Footer} from './Template'
+import BookForm from './BookForm'
+import {BrowserRouter as Router, Route, Navigate, Routes} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <main className ='container'>
-      <div className="App">
-        <Crud></Crud>
+    <Router>
+      <div className = "App">
+        <Navbar/>
+        <main className = "container">
+          <Routes>
+            <Route path = "/" element = {<Crud/>} />
+            <Route path = "/agregar-libro" element = {<BookForm/>} />
+            
+          </Routes>
+        </main>
+        <Footer/>
       </div>
-    </main>
+    </Router>
   );
 }
 
